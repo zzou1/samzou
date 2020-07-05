113f5091b504764a17eda020c75ca044ab04dbaf
zihao <zzou1@ucla.edu>
2020-07-04 17:55:50 -0700

    finished leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,7 +13,16 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+    /**check if a year is a leap year*/
+	public static boolean isLeapYear(int year){
+		if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
 
+
+	}
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
